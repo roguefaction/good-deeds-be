f84bcdfa83d28f462976f9f41dd03b6827e8a1b6
Vytautas Kondratas <vytas.k@live.com>
2019-03-03 19:47:33 +0200

    first commit

---
+++ src/test/java/com/example/gooddeedsbe/GoodDeedsBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gooddeedsbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodDeedsBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
