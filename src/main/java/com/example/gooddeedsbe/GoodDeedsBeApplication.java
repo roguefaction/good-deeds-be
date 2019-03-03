255cbc69df351d38c47600db75c8127149f09c66
Vytautas Kondratas <vytas.k@live.com>
2019-03-03 21:30:18 +0200

    testing heroku deployment

--- src/main/java/com/example/gooddeedsbe/GoodDeedsBeApplication.java
+++ src/main/java/com/example/gooddeedsbe/GoodDeedsBeApplication.java
@@ -3,6 +3,7 @@ package com.example.gooddeedsbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//spring application starter
 @SpringBootApplication
 public class GoodDeedsBeApplication {
 
