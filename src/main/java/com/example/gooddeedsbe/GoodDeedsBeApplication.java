bb158b21827d1ed9e9374efd1ebc4efcaf6bc0a6
juliusv <juliusvaliukas@gmail.com>
2019-03-03 21:20:39 +0200

    Added comment

--- src/main/java/com/example/gooddeedsbe/GoodDeedsBeApplication.java
+++ src/main/java/com/example/gooddeedsbe/GoodDeedsBeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoodDeedsBeApplication {
 
 	public static void main(String[] args) {
+		// test comment
 		SpringApplication.run(GoodDeedsBeApplication.class, args);
 	}
 
