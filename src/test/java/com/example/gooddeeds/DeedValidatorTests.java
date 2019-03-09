a839ae34801be2e51976ab5d8abe100c65d00a4e
Vytautas Kondratas <vytas.k@live.com>
2019-03-09 14:12:47 +0200

    removed unnecessary imports and annotations from DeedValidatorTests

--- src/test/java/com/example/gooddeeds/DeedValidatorTests.java
+++ src/test/java/com/example/gooddeeds/DeedValidatorTests.java
@@ -3,11 +3,8 @@ package com.example.gooddeeds;
 import com.example.gooddeeds.exceptions.*;
 import com.example.gooddeeds.utils.DeedValidator;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DeedValidatorTests {
 
