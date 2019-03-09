e764c0e2a1e26e1d029f89e9bd85c1d153857668
Vytautas Kondratas <vytas.k@live.com>
2019-03-09 10:56:01 +0200

    changed cors annotation to only accept requests from the front end app

--- src/main/java/com/example/gooddeedsbe/controller/DeedController.java
+++ src/main/java/com/example/gooddeedsbe/controller/DeedController.java
@@ -18,7 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://good-deeds-fe.herokuapp.com")
 @RestController
 public class DeedController {
     private DeedService deedService;
