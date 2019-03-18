9c63ebb641ca30fbcf767ed878ef1a4039b06907
Vytautas Kondratas <vytas.k@live.com>
2019-03-18 16:46:56 +0200

    quick fix for getting lists for unregistered users

--- src/main/java/com/example/gooddeeds/security/WebSecurity.java
+++ src/main/java/com/example/gooddeeds/security/WebSecurity.java
@@ -29,9 +29,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers(HttpMethod.POST, "/deeds").permitAll()
-                .antMatchers(HttpMethod.POST, "/upcomingdeeds").permitAll()
-                .antMatchers(HttpMethod.POST, "/allupcomingdeeds").permitAll()
+                .antMatchers(HttpMethod.GET, "/deeds").permitAll()
+                .antMatchers(HttpMethod.GET, "/upcomingdeeds").permitAll()
+                .antMatchers(HttpMethod.GET, "/allupcomingdeeds").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
