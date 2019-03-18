137aa35bd05a509a49fbd8ee347cfa2cc0f1d07c
Vytautas Kondratas <vytas.k@live.com>
2019-03-18 23:18:47 +0200

    added cors support for user controller

--- src/main/java/com/example/gooddeeds/controller/UserController.java
+++ src/main/java/com/example/gooddeeds/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
