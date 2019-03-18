1be07dbe2554ddd31cba377a959827aec656d377
Vytautas Kondratas <vytas.k@live.com>
2019-03-18 18:03:45 +0200

    testing fixes

--- src/main/java/com/example/gooddeeds/security/WebSecurity.java
+++ src/main/java/com/example/gooddeeds/security/WebSecurity.java
@@ -32,7 +32,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/deeds").permitAll()
                 .antMatchers(HttpMethod.GET, "/upcomingdeeds").permitAll()
                 .antMatchers(HttpMethod.GET, "/allupcomingdeeds").permitAll()
-                .antMatchers(HttpMethod.GET, "/allupcomingdeeds").authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
